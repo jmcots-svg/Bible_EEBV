cdd4141e4fbdfe79b0f814fee33c56f0e9289c28
jmcots-svg <jmcots@gmail.com>
2026-03-10 13:21:12 +0100

    Add files via upload

--- db.ts
+++ db.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: Deno.env.get("DATABASE_URL")
+});
 
 export default prisma;
