5b0cdce2ea0675e302cbfb3f051595b3aac94294
jmcots-svg <jmcots@gmail.com>
2026-03-10 13:15:21 +0100

    Add files via upload

--- db.ts
+++ db.ts
@@ -1,13 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const databaseUrl = Deno.env.get("DATABASE_URL");
-
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
-const prisma = new PrismaClient({
-  datasourceUrl: databaseUrl
-});
+const prisma = new PrismaClient();
 
 export default prisma;
