4bc8e36f3a514cb8fbf4871b984b836454f1c1cd
jmcots-svg <jmcots@gmail.com>
2026-03-10 13:36:09 +0100

    Add files via upload

--- db.ts
+++ db.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/client/deno/edge.ts";
+import { withAccelerate } from "npm:@prisma/extension-accelerate";
 
 const prisma = new PrismaClient({
-  datasourceUrl: Deno.env.get("DATABASE_URL")
-});
+  datasourceUrl: Deno.env.get("DATABASE_URL"),
+}).$extends(withAccelerate());
 
 export default prisma;
